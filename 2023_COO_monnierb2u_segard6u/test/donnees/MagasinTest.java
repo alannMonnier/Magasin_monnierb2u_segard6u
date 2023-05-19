7228cbf4e04a5a7fbe6dd75ba5bfecfadbfbfd1b
Alann <alann.monnier-blondeau9@etu.univ-lorraine.fr>
2023-05-19 10:35:54 +0200

    merge corriger

---
+++ 2023_COO_monnierb2u_segard6u/test/donnees/MagasinTest.java
@@ -0,0 +1,30 @@
+package donnees;
+
+import XML.ChargeurMagasin;
+import main.MainChargeurMagasin;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MagasinTest {
+
+    @Test
+    void trierAriste() throws IOException {
+        String repertoire = "musicbrainzSimple/";
+        ChargeurMagasin charge = new ChargeurMagasin(repertoire);
+        Magasin resultat = charge.chargerMagasin();
+
+        resultat.trierAriste();
+        for (int i =0; i<resultat.getNombreCds(); i++){
+                System.out.println(resultat.getListeCds().get(i).getNomCD());
+        }
+
+        assertEquals(true, true, "");
+    }
+
+    @Test
+    void trierAlbum() {
+    }
+}
